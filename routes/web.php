101bd65a5aaa441234b3b0a63930050e2b907eae
ayady-hamay-swara <245876666+ayady-hamay-swara@users.noreply.github.com>
2026-07-12 15:01:04 +0300

    first run on the front end

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('app');
 });
+Route::get('/login', function () {
+    return 'Login Page Placeholder';
+})->name('login'); // <--- This name string must exactly match 'login'
